8d30bd792a6571b40ed2a58a8894b519338b3134
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-30 01:44:30 +0200

    Clean up precode

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,12 @@
+/* eslint-disable space-before-function-paren */
+/* eslint-disable indent */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable no-tabs */
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '../../../../../vite.svg'
+
 import './App.css'
 
-function App () {
-  const [count, setCount] = useState(0)
-
-  return (
-		<div className='App'>
-			<div>
-				<a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://reactjs.org' target='_blank' rel='noreferrer'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount(count => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</div>
-  )
+function App() {
+	return <h1>Tiger Router</h1>
 }
 
 export default App
