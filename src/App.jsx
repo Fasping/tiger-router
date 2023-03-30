174c91f3343f125a409f1b8d772f8ec2006cd50b
Fernando Cases <nandocasesgarcia@gmail.com>
2023-03-31 00:07:35 +0200

    add: singel page render

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,57 @@
 /* eslint-disable no-tabs */
 
 import './App.css'
+import { useEffect, useState } from 'react'
+
+const NAVIGATION_EVENT = 'pushstate'
+
+function navigate(href) {
+	window.history.pushState({}, '', href)
+	// create custom event
+	const navigationEvent = new Event(NAVIGATION_EVENT)
+	window.dispatchEvent(navigationEvent)
+}
+
+function HomePage() {
+	return (
+		<>
+			<h1>Home</h1>
+			<p>Example page for create a React Router Clone.</p>
+			<button onClick={() => navigate('/about')}>Go to About us</button>
+		</>
+	)
+}
+
+function AboutPage() {
+	return (
+		<>
+			<h1>About</h1>
+			<p>Hey im Nando, and im creating a React Router Clone.</p>
+			<button onClick={() => navigate('/')}>Go Home</button>
+		</>
+	)
+}
 
 function App() {
-	return <h1>Tiger Router</h1>
+	const [currentPath, setCurrentPath] = useState(window.location.pathname)
+
+	useEffect(() => {
+		const onLocationChange = () => {
+			setCurrentPath(window.location.pathname)
+		}
+
+		window.addEventListener(NAVIGATION_EVENT, onLocationChange)
+		return () => {
+			window.removeEventListener(NAVIGATION_EVENT, onLocationChange)
+		}
+	}, [])
+
+	return (
+		<main>
+			{currentPath === '/' && <HomePage />}
+			{currentPath === '/about' && <AboutPage />}
+		</main>
+	)
 }
 
 export default App
