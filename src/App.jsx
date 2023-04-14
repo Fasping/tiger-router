a5213e9cd65cfa7403ac938c836e04c5a572da82
Fernando Cases <nandocasesgarcia@gmail.com>
2023-04-14 11:44:21 +0200

    Feat: add lazyload for home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { Suspense, lazy } from 'react'
 
 import './App.css'
 
-import HomePage from './pages/Home'
 import SearchPage from './pages/Search'
 import Page404 from './pages/Page404'
 
 import { Router } from './Router'
 import { Route } from './Route'
 
+const LazyHomePage = lazy(() => import('./pages/Home.jsx'))
 const LazyAboutPage = lazy(() => import('./pages/About.jsx'))
 
 const appRoutes = [
@@ -23,7 +23,7 @@ function App() {
 		<main>
 			<Suspense fallback={<div>Loading ...</div>}>
 				<Router routes={appRoutes} defaulComponent={Page404}>
-					<Route path='/' Component={HomePage} />
+					<Route path='/' Component={LazyHomePage} />
 					<Route path='/about' Component={LazyAboutPage} />
 				</Router>
 			</Suspense>
