c92b5a0d646211e1aa1e075145259e4c908cb800
Fernando Cases <nandocasesgarcia@gmail.com>
2023-04-15 13:50:41 +0200

    Feat: add i18n

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,30 @@
 import { Link } from '../Link'
 
-export default function AboutPage() {
+const i18n = {
+	es: {
+		title: 'Sobre Nosotros',
+		button: 'Ir a la Home',
+		description: 'Hola, soy Nando y estoy creando un React Router Clone.',
+	},
+	en: {
+		title: 'About us',
+		button: 'Go to Home page',
+		description: 'Hey im Nando, and im creating a React Router Clone.',
+	},
+}
+
+const useI18n = lang => {
+	return i18n[lang] || i18n.en
+}
+
+export default function AboutPage({ routeParams }) {
+	const i18n = useI18n(routeParams.lang ?? 'es')
+
 	return (
 		<>
-			<h1>About</h1>
-			<p>Hey im Nando, and im creating a React Router Clone.</p>
-			<Link to='/'>Go Home</Link>
+			<h1>{i18n.title}</h1>
+			<p>{i18n.description}</p>
+			<Link to='/'>{i18n.button}</Link>
 		</>
 	)
 }
